9ae6c51a7a113881f7aa0fee72005b2e9a884341
Ryan Koesters <rmkoesters@gmail.com>
2017-10-22 21:21:29 -0500

    Added debug mode to tweet function

--- tweet.go
+++ tweet.go
@@ -13,21 +13,27 @@ var (
 	consumerSecret = flag.String("consumer-secret", "QBu5KygNsDEsXJb7Q4ZlIObFsAHpkX3dHoNwQxmkDwSYrAkUYu", "")
 	accessToken    = flag.String("access-token", "917604710558953474-DBZCatP09K1PF6bjD9I1e7oZfzRTO8s", "")
 	accessSecret   = flag.String("access-secret", "MxX9jOMwX0tCegCJYh7SHHLOXM5FHFOth8NL7NozIf7lV", "")
+
+	live = flag.Bool("live", false, "send tweets to twitter instead of printing them to stdout")
 )
 
 func tweet(s string) {
+	if !*live {
+		log.Printf("tweet: %s", s)
+		return
+	}
+
 	config := oauth1.NewConfig(*consumerKey, *consumerSecret)
 	token := oauth1.NewToken(*accessToken, *accessSecret)
 
 	client := twitter.NewClient(oauth1.NewClient(context.TODO(), config, token))
 
 	params := &twitter.StatusUpdateParams{
-		Status: "hello, world",
+		Status: s,
 	}
 
-	t, _, err := client.Statuses.Update(params.Status, params)
+	_, _, err := client.Statuses.Update(params.Status, params)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Print(t.IDStr)
 }
