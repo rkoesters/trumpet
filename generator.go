ab343ba966ffb1789ef3ed88dc4a19ff02055b8c
Ryan Koesters <rmkoesters@gmail.com>
2017-10-25 20:16:32 -0500

    Added comments to generator.go

--- generator.go
+++ generator.go
@@ -1,5 +1,7 @@
 package main
 
+// Generator gets trained by calls to Train and creates strings of text
+// by calls to Generate.
 type Generator interface {
 	Train(s string)
 	Generate(maxLength uint) string
