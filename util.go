c89a84b238266c5aef09aee01c5951a372682d1f
Ryan Koesters <rmkoesters@gmail.com>
2017-10-25 20:24:05 -0500

    Added comments to util.go.

--- util.go
+++ util.go
@@ -1,5 +1,7 @@
 package main
 
+// isStringInSlice returns true if the string s is in slice of strings
+// slice, otherwise returns false.
 func isStringInSlice(s string, slice []string) bool {
 	for _, i := range slice {
 		if s == i {
