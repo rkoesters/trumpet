96fb054439799922adb13c8deb2e920b34919e8a
Ryan Koesters <rmkoesters@gmail.com>
2017-10-25 21:45:15 -0500

    Added util_test.go

---
+++ util_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIsStringInSlice(t *testing.T) {
+	slice := []string{
+		"test",
+		"in slice",
+		"test",
+	}
+
+	str1 := "in slice"
+	if !isStringInSlice(str1, slice) {
+		t.Fail()
+	}
+
+	str2 := "not in slice"
+	if isStringInSlice(str2, slice) {
+		t.Fail()
+	}
+}
