327454c101e907fa70464d14a4f0926b53a13c6e
Ryan Koesters <rmkoesters@gmail.com>
2018-03-16 23:31:07 -0500

    maxLength is inclusive.

--- generator/markov/markov.go
+++ generator/markov/markov.go
@@ -76,7 +76,7 @@ func (c *Chain) Generate(maxLength int) string {
 		numWords := maxLength / 6
 		words := c.GenerateWords(numWords)
 		text := strings.Join(words, " ")
-		if len(text) < maxLength {
+		if len(text) <= maxLength {
 			return text
 		}
 	}
