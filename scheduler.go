cfdb3b389ad16cf3449758a83d1de695e01ae263
Ryan Koesters <rmkoesters@gmail.com>
2018-03-18 04:28:52 -0500

    Add comment to scheduler.go.

--- scheduler.go
+++ scheduler.go
@@ -4,6 +4,8 @@ import (
 	"time"
 )
 
+// A Scheduler learns from calls to Train() and decides when by sending
+// a signal through Chan().
 type Scheduler interface {
 	Train(d time.Time)
 	Chan() <-chan struct{}
