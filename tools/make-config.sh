b4ab40a1b0dc74fe10033321ddd709455b471a69
Ryan Koesters <rmkoesters@gmail.com>
2018-03-17 23:49:03 -0500

    Added tool for generating config file.

---
+++ tools/make-config.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+usage() {
+	echo "Usage: $0 OUTPUT_FILE"
+}
+
+if [ $# -ne 1 ]; then
+	usage >&2
+	exit 1
+fi
+
+output_file="$1"
+
+printf 'Consumer Key: '
+read -r consumerkey
+printf 'Consumer Secret: '
+read -r consumersecret
+printf 'Access Token: '
+read -r accesstoken
+printf 'Access Secret: '
+read -r accesssecret
+
+{
+	echo "consumer-key=$consumerkey"
+	echo "consumer-secret=$consumersecret"
+	echo "access-token=$accesstoken"
+	echo "access-secret=$accesssecret"
+} >"$output_file"
