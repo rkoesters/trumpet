c8eb49c8aaaf46aea99e5d032bb3d72adfe87c44
Ryan Koesters <rmkoesters@gmail.com>
2018-03-18 04:21:02 -0500

    Tweak make-config.sh.

--- tools/make-config.sh
+++ tools/make-config.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 set -e
 
-usage() {
-	echo "Usage: $0 [OUTPUT_FILE]"
-}
-
 if [ $# -eq 0 ]; then
 	output_file="trumpet.conf"
 elif [ $# -eq 1 ]; then
 	output_file="$1"
 else
-	usage >&2
+	echo "Usage: $0 [OUTPUT_FILE]" >&2
 	exit 1
 fi
 
 printf 'Consumer Key: '
 read -r consumerkey
+
 printf 'Consumer Secret: '
 read -r consumersecret
+
 printf 'Access Token: '
 read -r accesstoken
+
 printf 'Access Secret: '
 read -r accesssecret
 
