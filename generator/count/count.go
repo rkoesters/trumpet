5a02c1f126b586776299527a1605694cba331d59
Ryan Koesters <rmkoesters@gmail.com>
2018-11-14 19:45:16 -0600

    Add comments to generator/count.

--- generator/count/count.go
+++ generator/count/count.go
@@ -2,16 +2,22 @@
 // number of times Train is called.
 package count
 
+// Generator is a trumpet.Generator that keeps track of the number of
+// times that Train is called.
 type Generator uint64
 
+// New returns a *Generator that keeps track of the number of times
+// Train is called.
 func New() *Generator {
 	return new(Generator)
 }
 
+// Train increments its counter.
 func (g *Generator) Train(s string) {
 	*g++
 }
 
+// Generate panics.
 func (g *Generator) Generate(maxLength int) string {
 	panic("count can't generate")
 }
