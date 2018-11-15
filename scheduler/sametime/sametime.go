22d3ab6930adffc89d278b1ea3717e2e4fe0e0a6
Ryan Koesters <rmkoesters@gmail.com>
2018-11-14 19:39:12 -0600

    Make scheduler/sametime.New return a trumpet.Scheduler.

--- scheduler/sametime/sametime.go
+++ scheduler/sametime/sametime.go
@@ -3,23 +3,26 @@
 package sametime
 
 import (
+	"github.com/rkoesters/trumpet"
 	"time"
 )
 
-type Scheduler struct {
+type scheduler struct {
 	ch chan struct{}
 }
 
-func New() *Scheduler {
-	return &Scheduler{
+// New returns a trumpet.Scheduler that schedules tweets to go out at
+// the same time Train is called.
+func New() trumpet.Scheduler {
+	return &scheduler{
 		ch: make(chan struct{}),
 	}
 }
 
-func (s *Scheduler) Train(d time.Time) {
+func (s *scheduler) Train(d time.Time) {
 	go func() { s.ch <- struct{}{} }()
 }
 
-func (s *Scheduler) Chan() <-chan struct{} {
+func (s *scheduler) Chan() <-chan struct{} {
 	return s.ch
 }
