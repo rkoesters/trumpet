026398c6a4178f6915951f082978bac6f77fc9fe
Ryan Koesters <rmkoesters@gmail.com>
2018-11-16 16:27:22 -0600

    Move command doc to usage.go.

--- cmd/trumpet-mkconfig/main.go
+++ cmd/trumpet-mkconfig/main.go
@@ -1,7 +1,3 @@
-// trumpet-mkconfig is an interactive trumpet configuration creation
-// tool. In addition to creating the configuration file, it also
-// verifies that the given credentials are valid and can be used to
-// connect to Twitter (this can be disabled with the -skip-verify flag).
 package main
 
 import (
--- cmd/trumpet-mkconfig/usage.go
+++ cmd/trumpet-mkconfig/usage.go
@@ -1,3 +1,11 @@
+// trumpet-mkconfig is an interactive trumpet configuration creation
+// tool. In addition to creating the configuration file, it also
+// verifies that the given credentials are valid and can be used to
+// connect to Twitter (this can be disabled with the -skip-verify flag).
+//
+// For usage information, run:
+//
+//	$ trumpet-mkconfig -help
 package main
 
 import (
